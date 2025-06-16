55161e49b83f17129b3b9d69dc51152108e75ed0
SUPERSTORE25 <ps3.superstore23@gmail.com>
2025-06-16 13:55:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const app = express();
 app.get("*", (req, res) => {
   const url = req.url;
   if (url.includes("ps3-updatelist.txt")) {
-    // Redireciona a requisição da Sony para o arquivo hospedado
-    res.redirect("https://update.superstoregames.com/ps3/list/ps3-updatelist.txt");
+    // Redireciona para o arquivo hospedado via HTTP
+    res.redirect("http://update.superstoregames.com/PS3/list/ps3-updatelist.txt");
   } else {
-    // Qualquer outra coisa, apenas bloqueia ou responde erro
     res.status(404).send("Arquivo não encontrado.");
   }
 });
