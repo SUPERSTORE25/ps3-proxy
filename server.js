4b5e19c3fd15b8008592cabcffd86d687bfe3bc2
SUPERSTORE25 <ps3.superstore23@gmail.com>
2025-06-16 16:02:26 -0300

    Update server.js
    4.0

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor proxy do PS3 está rodando na porta " + PORT);
 });
